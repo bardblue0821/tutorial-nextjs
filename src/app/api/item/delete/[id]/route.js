95177d008a70c3b5f6e5622f002cf2e3ec76d660
bardblue0821 <bardblue0821@gmail.com>
2025-05-13 16:47:05 +0900

    Add: アイテム削除用の DELETE エンドポイントを実装

---
+++ src/app/api/item/delete/[id]/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/utils/database";
+import { ItemModel } from "@/utils/schemaModels";
+
+export async function DELETE(request, context) {
+    const reqBody = await request.json();
+    try {
+        await connectDB();
+        const { id } = await context.params;
+        await ItemModel.deleteOne({ _id: id }, reqBody);
+        return NextResponse.json({message: "delete success"});
+    } catch {
+        return NextResponse.json({message: "delete failed"});
+    }
+}
