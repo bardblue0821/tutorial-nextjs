11bf1ab5010832bc5fd9a6eb67328ec8190caa7a
bardblue0821 <bardblue0821@gmail.com>
2025-05-15 13:06:49 +0900

    Add: JWT 認証用のミドルウェアを実装

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(request) {
+    console.log("Middleware is running");
+    const token = await request.headers.get("Authorization")?.split(" ")[1];
+    if (!token) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });   
+    }
+    try {
+        const secretKey = new TextEncoder().encode("next-market-app-book");
+        const decodedJwt = await jwtVerify(token, secretKey);
+        console.log("Decoded JWT:", decodedJwt);
+        return NextResponse.next();
+    } catch {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    
+}
+
+export const config = {
+    matcher: [
+        "/api/item/create", "/api/item/update/:path*", "/api/item/delete/:path*",
+    ],
+};
