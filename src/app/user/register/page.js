694b8df1a96ed9e7d307910f669ef6801bedd78a
bardblue0821 <bardblue0821@gmail.com>
2025-05-15 18:06:55 +0900

    Add: ユーザー登録フォームを実装し、名前の状態管理を追加

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -1,7 +1,35 @@
+"use client";
+import { useState } from "react";
+
 const Register = () => {
+    const [name, setName] = useState("");
+    const handleSubmit = () => {
+        try {
+            fetch("http://localhost:3000/api/user/register", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: "dummy"
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div>
             <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={(e) => {
+                    setName(e.target.value) 
+                    console.log(e)
+                }} type="text" name="name" placeholder="Name" required/>
+                <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button> register </button>
+            </form>
         </div>
     )
 }
