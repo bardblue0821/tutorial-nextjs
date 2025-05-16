885228ef66795c6e5f79478eb48666e8aa26493d
bardblue0821 <bardblue0821@gmail.com>
2025-05-16 10:13:45 +0900

    Add: ユーザー登録フォーム送信処理追加

--- src/app/user/register/page.js
+++ src/app/user/register/page.js
@@ -3,17 +3,27 @@ import { useState } from "react";
 
 const Register = () => {
     const [name, setName] = useState("");
-    const handleSubmit = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async(e) => {
+        e.preventDefault();
         try {
-            fetch("http://localhost:3000/api/user/register", {
+            const response = await fetch("http://localhost:3000/api/user/register", {
                 method: "POST",
                 headers: {
                     "Accept": "application/json",
                     "Content-Type": "application/json"
                 },
-                body: "dummy"
-            })
+                body: JSON.stringify({
+                    "name": name,
+                    "email": email,
+                    "password": password
+                })
+            });
+            const jsonData = await response.json();
+            alert(jsonData.message);
         } catch (error) {
+            alert("ユーザー登録失敗");
             console.log(error);
         }
     }
@@ -24,10 +34,15 @@ const Register = () => {
                 <input value={name} onChange={(e) => {
                     setName(e.target.value) 
                     console.log(e)
-                }} type="text" name="name" placeholder="Name" required/>
-                <input type="text" placeholder="Username" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                }} type="text" name="name" placeholder="名前" required/>
+                <input value={email} onChange={(e) => {
+                    setEmail(e.target.value) 
+                    console.log(e)
+                }} type="text" name="email" placeholder="メール" required/>
+                <input value={password} onChange={(e) => {
+                    setPassword(e.target.value) 
+                    console.log(e)
+                }} type="text" name="password" placeholder="パスワード" required/>
                 <button> register </button>
             </form>
         </div>
